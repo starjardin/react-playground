3acc570560e5b07be489b19ac9e221507271ca27
Tantely Andrianarivola <tantely.and@onja.org>
2020-10-14 08:36:32 +0300

    added a conditional function

--- conditional/App.js
+++ conditional/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
-import Conditional from './Conditional.js';
-/*
-Challenge:
+import React, { Component, useReducer } from 'react'
+
+/*Challenge:
 Given a stateless functional component:
 1. Follow the steps necessary to add state to it,
 2. Have state keep track of whether the user is logged in or not
@@ -9,30 +8,42 @@ Given a stateless functional component:
     a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
 4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
 */
-class App extends Component {
+
+function UserGreeting () {
+  return <h2>Welcom back!</h2>
+}
+
+function GuestGreeting () {
+  return <h2>Please sign in</h2>
+}
+
+function Greetings (props) {
+  const isLoggedIn = props.isLoggedIn
+  if (isLoggedIn) {
+    return <UserGreeting />
+  }
+  return <GuestGreeting />
+}
+
+export default class App extends Component {
   constructor () {
-    super ();
-    this.state = {
-      isLoggedIn : true,
-    }
-    this.handleState = this.handleState.bind(this);
+    super()
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleState () {
-    this.setState(prevState => {
-     return {
-        isLoggedIn : !prevState.isLoggedIn,
-     }
-    })
+  handleClick () {
+    console.log("Hello world");
   }
-
-  render ()  {
+  render () {
     return (
-          <Conditional 
-            handleState = {this.handleState}
-            user = {this.state}
-          />
+    <div>
+      <Greetings 
+        isLoggedIn={false}
+      />
+      <button onClick={this.handleClick}>
+        log
+      </button>
+    </div>
     )
   }
 }
-export default App
---
+++ conditional/AppLogin.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Conditional from './Conditional.js';
+/*
+Challenge:
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it,
+2. Have state keep track of whether the user is logged in or not
+3. Add a button that logs the user in/out
+    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+*/
+class AppLogin extends Component {
+  constructor () {
+    super ();
+    this.state = {
+      isLoggedIn : true,
+    }
+    this.handleState = this.handleState.bind(this);
+  }
+
+  handleState () {
+    this.setState(prevState => {
+     return {
+        isLoggedIn : !prevState.isLoggedIn,
+     }
+    })
+  }
+
+  render ()  {
+    return (
+          <Conditional 
+            handleState = {this.handleState}
+            user = {this.state}
+          />
+    )
+  }
+}
+export default App
