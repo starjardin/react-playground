2edd70e9f8c004ee944b189b979282c8239a5dbe
Tantely Andrianarivola <tantely.and@onja.org>
2020-10-16 08:35:10 +0300

    redid the meme generator project

---
+++ mm-generator/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h2>I am the header</h2>
+      </div>
+    )
+  }
+}
